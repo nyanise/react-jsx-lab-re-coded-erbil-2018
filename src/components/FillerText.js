0c56d8356987734f915b706d9741069114bdd69a
Neyan najat <nyannajat8@gmail.com>
2018-09-22 07:11:33 +0000

    automatically backed up by learn

---
+++ src/components/FillerText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class FillerText extends React.Component {
+  render() {
+    return (<h1> "The world's coolest webpage" </h1>)
+  }
+}
+
+
+export default  FillerText;
