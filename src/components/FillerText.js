82c4d04337a90a7caf875abc93ea33301361fb27
Neyan najat <nyannajat8@gmail.com>
2018-09-22 07:20:27 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -4,7 +4,7 @@ class FillerText extends React.Component
 {
   render()
   {
-    return (  <p>I am a filler text. I can be used to fill your screen. Amazing!</p>)
+    return  <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
   }
 }
 
